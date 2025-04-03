f28b1e1a56b134d5367c11a8faa440a2c418d3ea
unknown <mat.szczerek@gmail.com>
2025-04-03 22:05:22 +0200

    too tired to fix mistakes, but hey, at least it uses postgres now (kinda, cause not working lol)

--- backend/src/main/java/com/example/dpp/model/User.java
+++ backend/src/main/java/com/example/dpp/model/User.java
@@ -1,11 +1,21 @@
 package com.example.dpp.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public record User(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         Integer id,
+
+        @Column(name = "user_name", nullable = false, unique = true)
         String userName,
         String firstName,
         String lastName,
         String password,
+
+        @Enumerated(EnumType.STRING)
         Role role
 ) {
 }
