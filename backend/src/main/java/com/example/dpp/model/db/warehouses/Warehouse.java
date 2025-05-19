16a4380fba5a8de3026e8aaae458da7bb02c3560
MtszSid <mateusz.sidlo@hotmail.com>
2025-05-19 21:04:34 +0200

    restore

--- backend/src/main/java/com/example/dpp/model/db/warehouses/Warehouse.java
+++ backend/src/main/java/com/example/dpp/model/db/warehouses/Warehouse.java
@@ -18,7 +18,6 @@ public class Warehouse {
 
     @ElementCollection
     @Column(name = "products_list")
-    @ElementCollection
     private List<ProductsList> productsList;
 
     public Warehouse() {
