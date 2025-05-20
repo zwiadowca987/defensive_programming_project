05f5af7d1d293dda4fb6c5dff46db953d02918a6
Xarowsky <wiktorch@vp.pl>
2025-05-20 15:12:00 +0200

    register form stuff

--- frontend/src/app/components/CustomTextInput.tsx
+++ frontend/src/app/components/CustomTextInput.tsx
@@ -11,11 +11,22 @@ interface Props {
 export default function CustomTextInput(props:Props){
 
     const [field, meta] = useField(props.name)
-    
+ 
+    const warnStyle = {
+
+        color:'red'
+
+    }
+
     return (
-        <FloatingLabel label={props.label} controlId="floatingInput" className="mb-3">
-            <Form.Control {...field} {...props}/>
-        </FloatingLabel>
+        <>
+            <FloatingLabel label={props.label} controlId="floatingInput" className="mb-3">
+                <Form.Control {...field} {...props}/>
+            </FloatingLabel>
+            {meta.error && meta.touched ? (
+                <Form.Label style={warnStyle}>{meta.error}</Form.Label>
+            ) : null}
+        </>
     )
 
 }
--- frontend/src/app/components/registerForm.tsx
+++ frontend/src/app/components/registerForm.tsx
@@ -1,10 +1,11 @@
 import {observer} from "mobx-react-lite"
 import { useStore } from "../stores/stores"
-import { Field, Form, Formik } from "formik";
+import { Form, Formik } from "formik";
 import { Button } from "react-bootstrap";
+import CustomTextInput from "./CustomTextInput";
 import * as Yup from "yup"
 
-export default observer (function LoginForm(){
+export default observer (function registerForm(){
 
     const{userStore} = useStore();
 
@@ -13,20 +14,34 @@ export default observer (function LoginForm(){
         <Formik 
         initialValues={{email:'',userName:'',firstName:'',lastName:'', password:'', error:null}}
         validationSchema={Yup.object({
-            email:Yup.string().email().required(),
-            userName:Yup.string().required(),
-            firstName:Yup.string().required(),
-            lastName:Yup.string().required(),
-            password:Yup.string().length(8).required()
+            email:Yup.string()
+                .email("Podany mail nie jest prawidłowym adresem")
+                .required("E-mail jest wymagany"),
+            userName:Yup
+                .string()
+                .matches(/^[a-zA-Z0-9_]{3,20}$/,{message:"Nazwa użytkownika może składać się jedynie z liter"})
+                .required("Nazwa użytkownika jest wymagana"),
+            firstName:Yup
+                .string()
+                .matches(/^[a-zA-Zà-ÿÀ-Ÿ\s]{2,50}$/,{message:"Imię może składać się jedynie z liter"})
+                .required("Imię jest wymagane"),
+            lastName:Yup
+                .string()
+                .matches(/^[a-zA-Zà-ÿÀ-Ÿ\s]{2,50}$/,{message:"Nazwisko może składać się jedynie z liter"})
+                .required("Nazwisko jest wymagane"),
+            password:Yup.string()
+                .min(8)
+                .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s<>;'"&]).{8,}$/,{message:"Hasło musi zawierać wielką literę znak specjalny oraz liczbę"})
+                .required("Hasło jest wymagane")
         })}
         onSubmit={(values, {setErrors}) => userStore.register(values).catch(error => setErrors({error:"wrong password or e-mail"}))}>
             {({handleSubmit, isSubmitting, errors, isValid, dirty})=>(
                 <Form onSubmit={handleSubmit} autoComplete="off">
-                    <Field type="email" name="email"/>
-                    <Field name="userName" placeholder="User Name"/>
-                    <Field name="firstName" placeholder="First Name"/>
-                    <Field name="lastName" placeholder="Last Name"/>
-                    <Field type="password" name="password" placeholder="Password"/>
+                    <CustomTextInput type="email" label="E-mail" name="email" placeholder="a"/>
+                    <CustomTextInput name="userName" label="Nazwa użytkownika" placeholder="User Name"/>
+                    <CustomTextInput name="firstName" label="Imię" placeholder="First Name"/>
+                    <CustomTextInput name="lastName" label="Nazwisko" placeholder="Last Name"/>
+                    <CustomTextInput type="password" label="Hasło" name="password" placeholder="Password"/>
                     <Button type="submit" disabled={isSubmitting || !dirty || !isValid}>Register</Button>
                 </Form>
             )}
